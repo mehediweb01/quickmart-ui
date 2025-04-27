170d62a5028d005f012b967b3ac4e97278aba9fc
Md. Mehedi Hasan <mehedihasan0937580@gmail.com>
2025-04-27 14:53:33 +0600

    hero section done

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,9 @@
+import Hero from "@/component/hero";
+
 const Home = () => {
   return (
     <div>
-      <h1>Home page</h1>
+      <Hero />
     </div>
   );
 };
---
+++ src/component/hero.jsx
@@ -0,0 +1,26 @@
+const Hero = () => {
+  return (
+    <div
+      className="bg-center bg-no-repeat bg-cover h-screen md:h-[90vh] backdrop-blur-md backdrop-brightness-100 -z-50 flex justify-center items-center"
+      style={{ backgroundImage: "url('/images/hero-bg.jpg')" }}
+    >
+      <div className="px-3 sm:px-6 py-6 bg-white w-full mx-4 sm:w-[60%] space-y-6 text-center">
+        <h1 className="font-poppins font-medium text-3xl sm:text-[40px] leading-10 sm:leading-[57px] text-[#1D252C]">
+          The nature Candle
+        </h1>
+        <p className="text-[#1D252C] text-base sm:text-[18px] leading-[25px]">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam
+          saepe eaque repudiandae in repellat?
+        </p>
+        <button
+          type="button"
+          className="bg-green-800 hover:bg-green-600 hover:text-black tracking-[1px] text-white px-4 py-2 cursor-pointer text-base sm:text-xl font-medium font-roboto rounded-md transition-all duration-300"
+        >
+          Discovery our collection
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
